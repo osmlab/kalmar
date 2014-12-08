c93afe58a5b8a3b9e151cc9f08d638ec43d71a89
Aaron Lidman <aaronlidman@gmail.com>
2014-12-08 11:21:06 -0500

    More sudo

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 sudo apt-get update
-sudo apt-get install -y ruby-full rubygems imagemagick 
+sudo apt-get install -y git ruby-full rubygems imagemagick 
 sudo apt-get install -y postgresql-9.3 postgresql-9.3-postgis-2.1 postgresql-contrib-9.3 postgresql-client-9.3 postgresql-common postgresql-client-common postgresql-plpython-9.3
 sudo apt-get install -y zlib1g-dev libxml2-dev libpq-dev postgresql-server-dev-9.3
 sudo git clone --depth=1 https://github.com/openstreetmap/openstreetmap-website.git
 
 cd openstreetmap-website
-bundle install
+sudo bundle install
 
-cp config/example.application.yml config/application.yml
-cp config/example.database.yml config/databse.yml
+sudo cp config/example.application.yml config/application.yml
+sudo cp config/example.database.yml config/databse.yml
 
 sudo -u postgres -i
 createuser -s admin
@@ -18,15 +18,15 @@ psql -d osm_test -c "CREATE EXTENSION btree_gist;"
 exit
 
 cd db/functions
-make libpgosm.so
+sudo make libpgosm.so
 cd ../..
 
 # I know
 sudo chmod 777 /home/ubuntu
 
+sudo su postgres
 bundle exec rake db:migrate
-
 # open post 3000 on your instance
 bundle exec rails server
 # BAM: localhost:3000/
-# might need to hit it a few times initially things to cache
+# might need to hit it a few times initially for things to cache
