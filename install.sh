ba0422703936eed4babac2813b643203048a0557
Aaron Lidman <aaronlidman@gmail.com>
2014-12-08 11:23:06 -0500

    Seatbelts, gem install

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
+set -e -u
+
 sudo apt-get update
 sudo apt-get install -y git ruby-full rubygems imagemagick 
 sudo apt-get install -y postgresql-9.3 postgresql-9.3-postgis-2.1 postgresql-contrib-9.3 postgresql-client-9.3 postgresql-common postgresql-client-common postgresql-plpython-9.3
 sudo apt-get install -y zlib1g-dev libxml2-dev libpq-dev postgresql-server-dev-9.3
 sudo git clone --depth=1 https://github.com/openstreetmap/openstreetmap-website.git
 
+gem install bundle
 cd openstreetmap-website
 sudo bundle install
 
