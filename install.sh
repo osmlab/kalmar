9209beb5a43935cbb29c2e0420803c1938fb2e94
Aaron Lidman <aaronlidman@gmail.com>
2014-12-08 11:02:50 -0500

    Initial

---
+++ install.sh
@@ -0,0 +1,32 @@
+sudo apt-get update
+sudo apt-get install -y ruby-full rubygems imagemagick 
+sudo apt-get install -y postgresql-9.3 postgresql-9.3-postgis-2.1 postgresql-contrib-9.3 postgresql-client-9.3 postgresql-common postgresql-client-common postgresql-plpython-9.3
+sudo apt-get install -y zlib1g-dev libxml2-dev libpq-dev postgresql-server-dev-9.3
+sudo git clone --depth=1 https://github.com/openstreetmap/openstreetmap-website.git
+
+cd openstreetmap-website
+bundle install
+
+cp config/example.application.yml config/application.yml
+cp config/example.database.yml config/databse.yml
+
+sudo -u postgres -i
+createuser -s admin
+bundle exec rake db:create
+psql -d openstreetmap -c "CREATE EXTENSION btree_gist"
+psql -d osm_test -c "CREATE EXTENSION btree_gist;"
+exit
+
+cd db/functions
+make libpgosm.so
+cd ../..
+
+# I know
+sudo chmod 777 /home/ubuntu
+
+bundle exec rake db:migrate
+
+# open post 3000 on your instance
+bundle exec rails server
+# BAM: localhost:3000/
+# might need to hit it a few times initially things to cache
